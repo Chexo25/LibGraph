a3581f30e7242544da30e44c08e00e4650d4bfd0
Chexo <corentin.baille@etu.univ-tours.fr>
2025-03-23 16:10:05 +0100

    version commentée de CGraphOrient 0.0.3 23/03/2025 par Corentin

--- LibGraph/CGraphOrient.cpp
+++ LibGraph/CGraphOrient.cpp
@@ -1,1 +1,39 @@
 #include "CGraphOrient.h"
+
+void CGraphOrient::GORAjouterArc(CArc CArcGORNewArc) {
+
+}
+
+void CGraphOrient::GORModifierArc(CArc CArcGORArc, unsigned int uiGORSommetD, unsigned int uiGORSommetA) {
+
+}
+
+void CGraphOrient:: GORSupprimerArc(CArc CArcGORArc) {
+
+}
+
+void CGraphOrient::GORAjouterSommet(CSommet CSommetGORNewSommet) {
+
+}
+
+void CGraphOrient::GORModifierSommet(CSommet CSommetGORSommet, unsigned int uiGORNumSommet) {
+
+}
+
+void CGraphOrient::GORSupprimerSommet(CSommet CSommetGORSommet) {
+
+}
+
+CArc CGraphOrient::GORFindArc(unsigned int uiGORNumSommetD, unsigned int uiGORNumSommetA) {
+
+}
+
+CSommet CGraphOrient::GORFindSommet(unsigned int uiGORNumSommet) {
+
+}
+
+
+
+
+
+
--- LibGraph/CGraphOrient.h
+++ LibGraph/CGraphOrient.h
@@ -39,21 +39,21 @@ public :
 //METHODES
 
 /******************************************************************************************
- * GORAjouterCArc
- * --------------------------------------------------------------------------------------
- * Entrée : CArcGORNewArc, le nouvel arc que l'on veut ajouter au graphe
- * Nécessite : Qu'il n'existe pas déjà un arc ayant le même sommet de départ et d'arrivée
- * Que le sommet de départ et le sommet d'arrivée existent déjà dans le graphe
- * Sortie : Rien
- * Entraîne : L'arc spécifié est ajouté dans le graphe OU
- * (EXCEPTION): arc déjà existant OU
- * (EXCEPTION): le sommet de départ  ou d'arrivée n'existe pas
+	 * GORAjouterArc
+	 * --------------------------------------------------------------------------------------
+	 * Entrée : CArcGORNewArc, le nouvel arc que l'on veut ajouter au graphe
+	 * Nécessite : Qu'il n'existe pas déjà un arc ayant le même sommet de départ et d'arrivée
+	 * Que le sommet de départ et le sommet d'arrivée existent déjà dans le graphe
+	 * Sortie : Rien
+	 * Entraîne : L'arc spécifié est ajouté dans le graphe OU
+	 * (EXCEPTION): arc déjà existant OU
+	 * (EXCEPTION): le sommet de départ  ou d'arrivée n'existe pas
  ******************************************************************************************/
 
 	void GORAjouterArc(CArc CArcGORNewArc);
 
 /******************************************************************************************
-	* GORAjouterCArc
+	* GORModifierArc
 	* --------------------------------------------------------------------------------------
 	* Entrée : CArcGORArc, l'arc que l'on souhaite modifier, 
 	* uiGORSommetD, le nouveau sommet de départ de l'arc,
@@ -70,20 +70,78 @@ public :
 
 	void GORModifierArc(CArc CArcGORArc, unsigned int uiGORSommetD, unsigned int uiGORSommetA);
 
+/******************************************************************************************
+	* GORSupprimerArc
+	* --------------------------------------------------------------------------------------
+	* Entrée : CArcGORArc, l'arc que l'on souhaite supprimer,
+	* Nécessite : Que l'arc spécifié existe
+	* Sortie : Rien
+	* Entraîne : L'arc spécifié est supprimé dans le graphe OU
+	* (EXCEPTION): arc non existant
+******************************************************************************************/
+
 	void GORSupprimerArc(CArc CArcGORArc);
+
+/******************************************************************************************
+	 * GORAjouterSommet
+	 * --------------------------------------------------------------------------------------
+	 * Entrée : CSommetGORNewSommet, le nouveeau sommet que l'on veut ajouter au graphe
+	 * Nécessite : Rien
+	 * Sortie : Rien
+	 * Entraîne : Le sommet spécifié est ajouté dans le graphe
+ ******************************************************************************************/
 
 	void GORAjouterSommet(CSommet CSommetGORNewSommet);
 
+/******************************************************************************************
+	* GORModifierSommet
+	* --------------------------------------------------------------------------------------
+	* Entrée : CSommetGORSommet, le sommet que l'on souhaite modifier, 
+			   uiGORNumSommet, le nouveau numéro du sommet
+	* Nécessite : Que le nouveau numéro de sommet ne soit pas déjà attribué
+	* Sortie : Rien
+	* Entraîne : Le numéro du sommet est modifié OU
+	  (EXCEPTION): Le numéro de sommet a déjà été attribué
+******************************************************************************************/
+
 	void GORModifierSommet(CSommet CSommetGORSommet, unsigned int uiGORNumSommet);
+
+/******************************************************************************************
+	* GORSupprimerSommet
+	* --------------------------------------------------------------------------------------
+	* Entrée : CSommetGORSommet, le sommet que l'on souhaite supprimer,
+	* Nécessite : Que le sommet spécifié existe
+	* Que les arcs reliés à ce sommet soient aussi supprimés
+	* Sortie : Rien
+	* Entraîne : Le sommet spécifié est supprimé dans le graphe OU
+	* (EXCEPTION): sommet non existant OU
+	* (EXCEPTION): suppression des arcs impossibles
+******************************************************************************************/
 
 	void GORSupprimerSommet(CSommet CSommetGORSommet);
 
+/******************************************************************************************
+	* GORFindArc
+	* --------------------------------------------------------------------------------------
+	* Entrée : uiGORNumSommetD, le sommet de départ de l'arc recherché,
+	* uiGORNumSommetA, le sommet d'arrivée de l'arc recherché
+	* Nécessite : Rien
+	* Entraîne : L'affichage de l'arc recherché s'il existe, NULL sinon
+******************************************************************************************/
+
 	CArc GORFindArc(unsigned int uiGORNumSommetD, unsigned int uiGORNumSommetA);
+
+/******************************************************************************************
+	* GORFindSommet
+	* --------------------------------------------------------------------------------------
+	* Entrée : uiGORNumSommet, le sommet recherché,
+	* uiGORNumSommet, le numéro de sommet recherché
+	* Nécessite : Rien
+	* Entraîne : L'affichage du sommet recherché s'il existe, NULL sinon
+******************************************************************************************/
 
 	CSommet GORFindSommet(unsigned int uiGORNumSommet);
 };
 
 
 #endif
-
-//todo : faire les commentaires tkindt friendly
